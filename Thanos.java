9c6e4bbe1841c1fec5ac2f7407881e31805b9133
Berthold Voitl <berthold.voitl@telekom.de>
2019-12-06 12:15:50 +0100

    ok2

--- Thanos.java
+++ Thanos.java
@@ -8,41 +8,47 @@ public class Thanos {
         //TODO 1 : Create an empty heroes list
         ArrayList<Hero>fighter = new ArrayList<>();
 
-        Hero blackWidow = new Hero ("Black Widow", 34);
+        //Hero blackWidow = new Hero ("Black Widow", 34);
         Hero captainAmerica = new Hero ("Capatain America", 100);
         Hero vision = new Hero ("Vision", 3);
         Hero ironMan = new Hero ("Iron Man", 48);
         Hero scarletWitch = new Hero ("Scarlet Witch", 29);
-        Hero thor = new Hero ("Thor", 1500);
+        //Hero thor = new Hero ("Thor", 1500);
         Hero hulk = new Hero ("Hulk", 49);
         Hero doctorStrange = new Hero ("Doctor Strange", 42);
 
 
 
         // TODO 2 : Add those heroes to the list
-        fighter.add (blackWidow);
+        fighter.add (new Hero ("Black Widow", 34));
         fighter.add (captainAmerica);
         fighter.add (vision);
         fighter.add (ironMan);
         fighter.add (scarletWitch);
-        fighter.add (thor);
+        fighter.add (new Hero ("Thor", 1500));
         fighter.add (hulk);
         fighter.add (doctorStrange);
 
+
         // TODO 3 : It's Thor birthday, now he's 1501
-        thor.setAge (1501);
+        fighter.get(5).setAge (1501);
         // System.out.println(fighter.size());
-        Hero check = fighter.get(5);
+        //Hero check = fighter.get(5);
         //System.out.println(check.getAge());
 
         // TODO 4 : Shuffle the heroes list
         Collections.shuffle(fighter);
 
-        //TODO 5 : Keep only the half of the list
-        for (int k = 0; k < fighter.size()/2+2; k++){
+        /*//TODO 5 : Keep only the half of the list
+        for (int k = 0; k <= fighter.size()/2+1; k++){
           //System.out.println("-" + fighter.get(k).getName());
+
           fighter.remove(k);
-        }
+        }*/
+        /*List<Hero>sublist = fighter.subList(0, fighter.size()/2);
+        sublist.clear();*/
+        fighter.subList(0, fighter.size()/2).clear();
+        //System.out.println (sublist);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
           for (int i = 0; i < fighter.size(); i++) {
